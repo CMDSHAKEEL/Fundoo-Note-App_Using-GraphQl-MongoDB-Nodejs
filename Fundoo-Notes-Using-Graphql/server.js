4dad048b4b1b99ddfdbde4a3de303a520d346eef
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-09-27 18:05:43 +0530

    Welcome-To-Fundoo-Notes-App-Using_Graphql-MongoDB-Nodejs

---
+++ Fundoo-Notes-Using-Graphql/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT =8000;
+ 
+const app = express();
+app.use(bodyParser.json());
+
+app.listen(PORT ,()=>{
+    console.log(`server is listening port ${PORT}`)
+    });
