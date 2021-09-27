8e1bb5961b0e6e7ab4662b13bf5b94575ee2057f
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-09-27 18:17:48 +0530

    feat:- connecting to MongoDB (database)

--- Fundoo-Notes-Using-Graphql/server.js
+++ Fundoo-Notes-Using-Graphql/server.js
@@ -5,6 +5,22 @@ const PORT =8000;
 const app = express();
 app.use(bodyParser.json());
 
+// Configuring the database
+const dbConfig = require('./config/database.config');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
 app.listen(PORT ,()=>{
     console.log(`server is listening port ${PORT}`)
     });
