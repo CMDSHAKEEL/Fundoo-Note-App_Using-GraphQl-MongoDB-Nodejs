8d7f315f69c70b47bb4d0f5e9785db2dd3173db1
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-09-29 08:43:54 +0530

    feat:- Adding to data to Database and retriving data to Graphql in http local host

--- Fundoo-Notes-Using-Graphql/app/WorkingofGraphql/resolver/resolvers.js
+++ Fundoo-Notes-Using-Graphql/app/WorkingofGraphql/resolver/resolvers.js
@@ -5,7 +5,16 @@ const userResolver ={
 
     
     events:()=>{
-     return  events;
+     return Event.find()
+     .then(events=>{
+        return events.map(event=>{
+            return{ ...event._doc};
+        })
+         }
+     )
+     .catch(err=>{
+         throw err;
+     });
       
     },
     // Creating User
@@ -26,9 +35,28 @@ const userResolver ={
               console.log(err);
               return err;
           })
+    },
+    // Login 
+    
+    loginUser:  args =>{
+        return bcrypt
+         .hash(args.userInput.password,  12)
+         .then(hashedpassword=>{
+             const user = new User({
+                 email: args.userInput.email,
+                 password: hashedpassword,
+             })
+             return user.save();
+         })
+         .then(result =>{
+             return { ...result._doc, _id: result._id}
+         })
          
-  
-    }
+         .catch(err =>{
+             throw err;
+         })
+         
+     }
 }
 
 module.exports = userResolver; 
