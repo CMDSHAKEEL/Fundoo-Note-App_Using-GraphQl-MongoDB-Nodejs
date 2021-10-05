40bcd50bda40736266151c980f4522d127e9a679
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-10-05 22:46:34 +0530

    refactor:-database connection refactoring from server.js to database.config.js

--- Fundoo-Notes-Using-Graphql/config/database.config.js
+++ Fundoo-Notes-Using-Graphql/config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: `mongodb://localhost:27017/Cmdshakeel21`
+    url: `mongodb://localhost:27017/Cmdshakeel123`
 }
--- Fundoo-Notes-Using-Graphql/server.js
+++ Fundoo-Notes-Using-Graphql/server.js
@@ -6,20 +6,9 @@ const  buildSchema     =  require('./app/WorkingofGraphql/Schema/index')
 const userResolvers    =  require('./app/WorkingofGraphql/resolver/index')
 const app              =  express();
 app.use(bodyParser.json());
-// Configuring the database 
-const dbConfig         =  require('./config/database.config');
-const mongoose         =  require('mongoose');
-mongoose.Promise       =  global.Promise;
+ const dbConfig = require('./config/database.configs')
 
-// Connecting to the database
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Successfully connected to the database");    
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
+dbConfig.dbConnection();
 
 //working of graphql
 
@@ -31,5 +20,5 @@ app.use('/graphql', graphqlHTTP({
 );
 
 app.listen(PORT ,()=>{
-    console.log(`server is listening port ${PORT}`)
+    console.log(`server is listening port 3000`)
     });
