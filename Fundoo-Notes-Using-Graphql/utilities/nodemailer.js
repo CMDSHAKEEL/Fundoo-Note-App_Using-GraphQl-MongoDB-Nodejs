4837114b7aeebc38c8ecb5b820080eca6e8e7835
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-10-05 19:15:45 +0530

    feat:-nodemailer is added for forgot password

--- Fundoo-Notes-Using-Graphql/utilities/nodemailer.js
+++ Fundoo-Notes-Using-Graphql/utilities/nodemailer.js
@@ -1,1 +1,25 @@
- 
+  
+const nodemailer = require('nodemailer');
+code = Math.random().toString(33).substring(3, 17)  
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth:{
+        user:'cmdshakeel21@gmail.com',
+        pass:'db21051998'
+    }
+});
+
+let mailoptions = {
+    from:'cmdshakeel21@gmail.com',
+    to:'cmdshakeel3377@gmail.com', 
+    subject:'testing',
+    text:code
+};
+
+transporter.sendMail(mailoptions, function(err,data){
+    if(err){
+        console.log(err)
+    }else{
+        console.log(data)
+    }
+})
